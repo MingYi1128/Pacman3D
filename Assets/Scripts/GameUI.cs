7b486f45907e8dbfbfedc6b1436ad40a1be8f6cb
M <59655429+MingYi1128@users.noreply.github.com>
2026-01-02 21:38:09 +0800

    feat: GameUI API

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,7 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 
 public class GameUI : MonoBehaviour
 {
 
+    [SerializeField] private TextMeshProUGUI _pelletCounterText;
+    [SerializeField] private TextMeshProUGUI _levelText;
+    [SerializeField] private TextMeshProUGUI _lifeText;
+
+
+    public void setLife(int life)
+    {
+        _lifeText.text = life.ToString();
+    }
+
+    public void setLevel(int level)
+    {
+        _levelText.text = "Lvl " + level.ToString();
+    }
+
+
+    public void setPelletCount(int currentPelletCount, int totalPelletCount)
+    {
+        _pelletCounterText.text = string.Format("%d/%d", currentPelletCount);
+    }
 }
